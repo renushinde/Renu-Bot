39c1d0da4a6b3ed6f1189fcc3b97b44d083da1e2
Renu Shinde <renunikhilp@gmail.com>
2018-02-27 12:20:25 -0800

    hubot test

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,7 @@ module.exports = function(robot) {
   robot.respond(/What's your favorite food?/, function(res) {
   return res.send("I'm a robot--I don't eat food!");
   });
-  
+
   robot.hear(/Hello!/, function(res) {
   return res.send("Hi there!");
 });
@@ -29,6 +29,36 @@ module.exports = function(robot) {
  //  return msg.send("Hey There!");
   // });
 }
+module.exports = function(bot) {
+
+
+    //Catches  'curse' words and responds with an attempt at being amusing
+    bot.hear(/(damn.*)|(shit.*)|(hell)/i, function (msg) {
+        if(msg.match[1]){
+          return msg.send("It's getting real PG in here");
+        }
+        else if(msg.match[2]){
+            return msg.send("Excuse your language!");
+        }
+        else if(msg.match[3]){
+            return msg.send("Oh heck no");
+        }
+    })
+
+    //Catches a common misspelling of embarrass and 'embarrasses' people by calling them out
+    bot.hear(/(embarass me|embarass)/i, function(msg) {
+            return msg.reply("*embarrass..idiot")
+    })
+
+    //Assigns array of different responses to a variable 'looks'
+    var looks = ['like a 1','like a 5','like a 10', 'like Gods most wondrous creature','fug','Scrumtrulescent: definition- so great that any other word employed would be woefully insufficient, and would serve only to limit the sheer magnitude of the greatness intended as a descriptor']
+
+    //Responds to the question: 'How do I look today?'and invokes msg.random with the 'looks' array as a parameter. 
+    bot.respond(/(How do I look today\?)/i, function(msg) {
+         return msg.send(msg.random(looks));
+     })
+}
+
 
 /************************************
 
