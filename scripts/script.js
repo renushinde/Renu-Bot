821dfaee63043dde0c116310b3677088d4976b1e
Renu Shinde <renunikhilp@gmail.com>
2018-03-05 12:24:35 -0800

    hubot changes

--- scripts/script.js
+++ scripts/script.js
@@ -30,11 +30,11 @@
   // });
 }
 */
-module.exports = function(bot) {
-
+//module.exports = function(robot) {
+ /*
 
     //Catches  'curse' words and responds with an attempt at being amusing
-    bot.hear(/(damn.*)|(shit.*)|(hell)/i, function (msg) {
+    robot.hear(/(damn.*)|(shit.*)|(hell)/i, function (msg) {
         if(msg.match[1]){
           return msg.send("It's getting real PG in here");
         }
@@ -50,14 +50,20 @@ module.exports = function(bot) {
     bot.hear(/(embarass me|embarass)/i, function(msg) {
             return msg.reply("*embarrass..idiot")
     });
+    */
 
     //Assigns array of different responses to a variable 'looks'
-    var looks = ['like a 1','like a 5','like a 10', 'like Gods most wondrous creature','fug','Scrumtrulescent: definition- so great that any other word employed would be woefully insufficient, and would serve only to limit the sheer magnitude of the greatness intended as a descriptor']
+    module.exports = function(robot) {
+    var looks = ['like a 1','like a 5','like a 10', 'like Gods most wondrous creature','Beautiful','Scrumtrulescent: definition- so great that any other word employed would be woefully insufficient, and would serve only to limit the sheer magnitude of the greatness intended as a descriptor']
 
     //Responds to the question: 'How do I look today?'and invokes msg.random with the 'looks' array as a parameter. 
-    bot.respond(/(How do I look today\?)/i, function(msg) {
+    robot.hear(/(How do I look today\?)/i, function(msg) {
          return msg.send(msg.random(looks));
      });
+
+     robot.hear(/(embarass me|embarass)/i, function(msg) {
+            return msg.reply("*It's spelled as embarrass..");
+    });
 }
 
 
